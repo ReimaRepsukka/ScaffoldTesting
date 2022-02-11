612c6c8e519804164445804defa825eac61a02da
repsukka <81526112+repsukka@users.noreply.github.com>
2022-02-11 10:35:20 +0200

    Drawer

--- app/src/main/java/com/example/scaffoldtesting/Testing.kt
+++ app/src/main/java/com/example/scaffoldtesting/Testing.kt
@@ -6,42 +6,76 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainView() {
 
 
     val navController = rememberNavController()
+    val scState = rememberScaffoldState( rememberDrawerState(DrawerValue.Closed) )
 
     Scaffold(
-        topBar = { MyTopBar() },
+        scaffoldState = scState,
+        topBar = { MyTopBar(scState) },
         bottomBar = { MyBottomBar(navController) },
+        drawerContent = { MyDrawerLayout(navController,scState) }
     ){
         NavHost(navController = navController, startDestination = "wifi"){
             composable(route="wifi"){
-                Box(modifier = Modifier.fillMaxSize().background(Color.Cyan))
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Cyan))
             }
             composable(route="home"){
-                Box(modifier = Modifier.fillMaxSize().background(Color.LightGray))
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray))
             }
             composable(route="music"){
-                Box(modifier = Modifier.fillMaxSize().background(Color.Yellow))
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Yellow))
             }
         }
     }
 }
 
 @Composable
-fun MyTopBar() {
+fun MyDrawerLayout(navContoller: NavHostController,scState: ScaffoldState) {
+    val scope = rememberCoroutineScope()
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+    ) {
+        Text(
+            text = "Wifi setting",
+            fontSize = 26.sp,
+            modifier = Modifier.clickable {
+                navContoller.navigate("wifi")
+                scope.launch {
+                    scState.drawerState.close()
+                }
+            })
+    }
+}
+
+@Composable
+fun MyTopBar(scState: ScaffoldState) {
+
+    val scope = rememberCoroutineScope()
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -52,7 +86,12 @@ fun MyTopBar() {
     ) {
         Icon(
             painter = painterResource(R.drawable.ic_baseline_menu_24),
-            contentDescription = "")
+            contentDescription = "",
+            modifier = Modifier.clickable {
+                scope.launch {
+                    scState.drawerState.open()
+                }
+            })
         OutlinedTextField(
             value = "",
             onValueChange = {},
